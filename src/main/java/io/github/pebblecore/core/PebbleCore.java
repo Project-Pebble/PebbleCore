ae18aa96efc0d2d962df6ca227c0f52dc5da9fd5
Tastac <keyganap@gamil.com>
2020-02-23 23:08:26 -0800

    First Commit
    Added Forge
    Basic 1.15.2 Mod Setup Finished

---
+++ src/main/java/io/github/pebblecore/core/PebbleCore.java
@@ -0,0 +1,10 @@
+package io.github.pebblecore.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PebbleCore.MOD_ID)
+public class PebbleCore {
+
+    public static final String MOD_ID = "pebblecore";
+
+}
